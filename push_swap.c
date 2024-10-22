0ccc6de041bca5f09a8255071631df00b13a4ffe
Shugo Ishige <sishige@c3r6s1.42tokyo.jp>
2024-10-22 21:46:46 +0900

    ADD ulong_to_ternary(), parse_sign(), safe_atoi(), make_node()

--- push_swap.c
+++ push_swap.c
@@ -6,15 +6,111 @@
 /*   By: sishige <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 21:33:58 by sishige           #+#    #+#             */
-/*   Updated: 2024/10/18 22:08:44 by sishige          ###   ########.fr       */
+/*   Updated: 2024/10/22 21:42:18 by sishige          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+char	*ulong_to_ternary(char *ternary, unsigned long ul)
+{
+	int		len;
+	size_t	base_len;
+
+	ft_memset(ternary, '-', INT_TERNARY_MAX_LEN - 1);
+	len = INT_TERNARY_MAX_LEN - 1;
+	ternary[len--] = '\0';
+	if (ul == 0)
+		return (ternary);
+	base_len = ft_strlen(TER_DIGITS);
+	while (0 <= len)
+	{
+		if (0 < ul)
+		{
+			ternary[len--] = TER_DIGITS[ul % base_len];
+			ul /= base_len;
+		}
+		else
+			ternary[len--] = '0';
+	}
+	return (ternary);
+}
+
+char	*parse_sign(char *str, int *sign)
+{
+	while (ft_isspace(*str))
+		str++;
+	*sign = 1;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			*sign = -1;
+		str++;
+	}
+	return (str);
+}
+
+int	safe_atoi(int *n, char *str)
+{
+	int			sign;
+	long long	lln;
+
+	str = parse_sign(str, &sign);
+	lln = 0;
+	while (ft_isdigit(*str))
+	{
+		lln = lln * 10 + (*str - '0');
+		str++;
+		if (ft_isdigit(*str) && sign == 1 && ((INT_MAX - (*str - '0'))
+				/ 10 < lln || lln * 10 >= INT_MAX))
+			return (1);
+		if (ft_isdigit(*str) && sign == -1 && ((INT_MIN + (*str - '0'))
+				/ 10 > -lln || lln * 10 < INT_MIN))
+			return (1);
+	}
+	*n = sign * (int)lln;
+	return (0);
+}
+
+//t_contents	*make_contents(a)
+
+t_list	*make_node(char	*argv)
+{
+	t_list		*new_node;
+	t_contents	*new_contents;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (new_node == NULL)
+		return (NULL);
+	new_contents = (t_contents *)malloc(sizeof(t_contents));
+	if (new_contents == NULL)
+		return (NULL);
+	new_contents->str = argv;
+	if (safe_atoi(&new_contents->num, argv))
+		return (NULL);
+	new_node->content = new_contents;
+	return (new_node);
+}
+
 void	init_stack_a(t_list *stack_a, int argc, char *argv[])
 {
-	
+	t_list	*sentinel;
+	size_t	i;
+
+	sentinel = stack_a;
+	i = 0;
+	while (i < (size_t)argc - 1)
+	{
+		stack_a->next = make_node(argv[i]);
+		if (stack_a->next == NULL)
+		{
+			ft_lstclear(&sentinel, &free);
+			die("malloc error");
+		}
+		stack_a->next->prev = stack_a;
+		stack_a = stack_a->next;
+		i++;
+	}
 }
 
 void	init_stacks(t_list *stack_a, t_list *stack_b, int argc, char *argv[])
@@ -34,12 +130,13 @@ void	print_lst(char const *str, t_list *lst)
 			str, lst->content, lst, lst->prev, lst->next);
 }
 
-int	main(int argc, char *argv[])
+int	main(void)
+//int	main(int argc, char *argv[])
 {
-	t_list	stack_a;
-	t_list	stack_b;
+	t_list	*node;
 
-	init_stacks(&stack_a, &stack_b, argc, argv);
+	node = make_node("1234567890");
+	print_lst("node", node);
 	return (0);
 }
 
--- push_swap.h
+++ push_swap.h
@@ -6,20 +6,26 @@
 /*   By: sishige <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 21:10:13 by sishige           #+#    #+#             */
-/*   Updated: 2024/10/18 21:34:40 by sishige          ###   ########.fr       */
+/*   Updated: 2024/10/22 20:28:28 by sishige          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# define INT_TERNARY_MAX_LEN 22
+# define TER_DIGITS "012"
+
 # include "libft/libft.h"
 
 typedef struct s_contents
 {
 	char			*str;
+	int				num;
 	unsigned long	map;
-	char			ternary[22];
+	char			ternary[INT_TERNARY_MAX_LEN];
 }				t_contents;
+
+void	die(char *cause);
 
 #endif
