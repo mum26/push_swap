44ed63741b1b54b082ede06da4d5eefe4ae34f9d
Shugo Ishige <sishige@c3r3s1.42tokyo.jp>
2024-10-30 21:57:38 +0900

    ADD swap_content(). contentだけ入れ替えればいいことに気がついた。

--- swap.c
+++ swap.c
@@ -6,38 +6,55 @@
 /*   By: sishige <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 20:06:38 by sishige           #+#    #+#             */
-/*   Updated: 2024/10/29 20:34:30 by sishige          ###   ########.fr       */
+/*   Updated: 2024/10/30 20:44:28 by sishige          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	swap(t_list **lst, char *str)
+void		swap_content(t_list *a, t_list *b, char *str)
 {
-	t_list	*temp;
+	void	*temp;
 
-	temp = *lst;
-	*lst = (*lst)->next;
-	(*lst)->prev = temp->prev;
-	temp->prev = *lst;
-	temp->next = (*lst)->next;
-	(*lst)->next = temp;
+	temp = a->content;
+	a->content = b->content;
+	b->content = temp;
+	if (str)
+		ft_printf("%s\n", str);
+}
+
+static void	swap_head_and_second(t_list **lst, char *str)
+{
+	t_list	*sentinel;
+	t_list	*head;
+	t_list	*second;
+
+	sentinel = *lst;
+	head = sentinel->next;
+	second = head->next;
+	if (sentinel == second)
+		return ;
+	sentinel->next = second;
+	head->prev = second;
+	head->next = second->next;
+	second->prev = sentinel;
+	second->next = head;
 	if (str)
 		ft_printf("%s\n", str);
 }
 
 void	swap_a(t_list **stack_a)
 {
-	swap(stack_a, "sa");
+	swap_head_and_second(stack_a, "sa");
 }
 
 void	swap_b(t_list **stack_b)
 {
-	swap(stack_b, "sb");
+	swap_head_and_second(stack_b, "sb");
 }
 
 void	sa_and_sb(t_list **stack_a, t_list **stack_b)
 {
-	swap(stack_a, NULL);
-	swap(stack_b, "ss");
+	swap_head_and_second(stack_a, NULL);
+	swap_head_and_second(stack_b, "ss");
 }
