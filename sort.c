7d2e0d44f809ae064f4f05075c9fb5a1c5994b4d
Shugo Ishige <sishige@c3r1s1.42tokyo.jp>
2024-11-01 19:03:10 +0900

    CREAT sort.c

---
+++ sort.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sishige <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 18:43:36 by sishige           #+#    #+#             */
+/*   Updated: 2024/11/01 19:01:23 by sishige          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	radix_sort(t_list **stack_a, int argc)
+{
+	t_list	*stack_b;
+
+	make_lst(&stack_b);
+	if (stack_b == NULL)
+		return ;
+	if (argc == 2)
+	if (argc == 3)
+	if (argc == 4)
+	if (argc == 5)
+	if (argc == 6)
+	if (argc == 7)
+	if (7 < argc)
+		exec_sort(stack_a, get_num_of_digit_base(argc - 1, TER_DIGITS));
+}
