e4342bfa4e70e2d58a46d723835285a3f6c7a93a
Shugo Ishige <sishige@c3r6s7.42tokyo.jp>
2024-11-05 20:36:45 +0900

    push nodeが0, 1だった場合を追加

--- push.c
+++ push.c
@@ -6,7 +6,7 @@
 /*   By: sishige <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 20:04:01 by sishige           #+#    #+#             */
-/*   Updated: 2024/11/03 22:35:36 by sishige          ###   ########.fr       */
+/*   Updated: 2024/11/05 16:19:51 by sishige          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,32 @@ static void	push_left_to_right(t_list **left, t_list **right, char *str)
 {
 	t_list	*lst[LR];
 	t_list	*head[LR];
+	t_list	*next[LR];
+	t_list	*tail[LR];
 
 	lst[L] = *left;
+	head[L] = lst[L]->next;
+	next[L] = head[L]->next;
+	tail[L] = lst[L]->prev;
+
 	lst[R] = *right;
+	head[R] = lst[R]->next;
+	next[R] = head[R]->next;
+	tail[R] = lst[R]->prev;
+
 	if (lst[L] == lst[L]->next)
 		return ;
-	head[L] = lst[L]->next;
-	head[R] = lst[R]->next;
 
-	lst[L]->next = head[L]->next;
-	head[L]->next->prev = lst[L];
+	lst[L]->next = next[L];
+	next[L]->prev = lst[L];
+	if (next[L] == lst[L])
+		lst[L]->prev = lst[L];
 
 	lst[R]->next = head[L];
 	head[L]->prev = lst[R];
 	head[L]->next = head[R];
-	head[R]->prev = head[L];
+	if (tail[R] == lst[R])
+		lst[R]->prev = head[L];
 
 	if (str)
 		ft_printf("%s\n", str);
